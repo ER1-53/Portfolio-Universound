4d49ce532f20b7e17c74899542a48362b6187382
ER1-53 <cremey.erwan@gmail.com>
2024-01-28 21:04:10 +0100

    create all file and folder for first pat of project

--- REACT-UNIVERSOUND/src/app.tsx
+++ REACT-UNIVERSOUND/src/app.tsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import Song from './models/song';
+import SONGS from './models/mock-song';
+import SongListHistoric from './components/main/song-list-historic';
+/*import SongLexicalFieldList from './components/main/song-lexical-field-list';
+import SongTypeList from './components/main/song-type-list';
+import SongRhithmList from './components/main/song-rhithm-list';*/
   
-const App: React.FC = () => {
- const name: String = 'React';
-    
+const App: FunctionComponent = () => {
+    const [songs, setsongs] = useState<Song[]>([]);
+
+    useEffect(() => {
+        setsongs(SONGS);
+    }, []);
  return (
-  <h1>Hello, {name} !</h1>
- )
+    <div className="App">
+    <header>
+      <div className="title">
+        <div>
+          <div className="logo logo_header"></div>
+        </div>
+        <nav className="nav_profil">
+          <ul className="nav_profil_ul">
+            <li><a href="./loader_page.html"><i className="fa-regular fa-user fa-lg">Account</i></a></li>
+            <li><a href="./signup.html"><i className="fa-solid fa-right-to-bracket fa-lg">Sign Up</i></a></li>
+            <li><a href="./sounds_page.html"><i className="fa-solid fa-right-to-bracket fa-lg">Standard</i></a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <div className="big_box">
+      <aside className="aside">
+        <nav className="nav">
+          <ul className="nav_ul">
+            <li><i className="fa-solid fa-house-crack"></i> Home</li>
+            <li><i className="fa-regular fa-bookmark"></i> Library</li>
+            <li><i className="fa-solid fa-music"></i> Playlist</li>
+          </ul>
+          <div>
+            Place ici le system de chat
+          </div>
+        </nav>
+      </aside>
+      <main className="main">
+        <div className="searchBox">
+          <form>
+              <input className="styledSearch searchField" type="search" id="site-search" name="request" />
+              <button className="styledSearch">Search</button>
+          </form>
+        </div>
+        <SongListHistoric />
+        <section className="yourStyle">
+        </section>
+      </main>
+    </div>
+    <footer className="footer">
+      <nav className="nav_footer">
+        <hr />
+        <div className="logo logo_footer"></div>
+        <hr />
+        <ul className="RGPD box_footer">
+          <li>General Condition</li>
+          <li>Gestion des Cookies</li>
+          <li>Mentions légales</li>
+        </ul>
+        <hr />
+        <ul className="user box_footer">
+          <li>Profil</li>
+          <li>Last Sound</li>
+          <li>Playlist</li>
+        </ul>
+        <hr />
+        <ul className="contact box_footer">
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Inovation</li>
+        </ul>
+        <hr />
+      </nav>
+      <p>create By Team Sound for Holberton</p>
+    </footer>
+  </div>
+ );
 }
   
 export default App;
---
+++ REACT-UNIVERSOUND/src/models/song.ts
@@ -0,0 +1,34 @@
+export default class Song {
+    // 1. Typage des propiétés d'un pokémon.
+    id: number;
+    track: string;
+    album: string;
+    time: number;
+    picture: string;
+    video: string;
+    types: Array<string>;
+    created: Date;
+    
+     
+    // 2. Définition des valeurs par défaut des propriétés d'un pokémon.
+    constructor(
+     id: number,
+     track: string = 'track',
+     album: string = 'album',
+     time: number = 200,
+     picture: string = 'http://...',
+     video: string = 'http://...',
+     types: Array<string> = ['Rock'],
+     created: Date = new Date()
+    ) {
+     // 3. Initialisation des propiétés d'un pokémons.
+     this.id = id;
+     this.track = track;
+     this.album = album;
+     this.time = time;
+     this.picture = picture;
+     this.video = video;
+     this.types = types;
+     this.created = created;
+    }
+   }
