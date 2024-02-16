ebf86a64d6c99431290fa764b2c7c9c9be4507e8
ER1-53 <cremey.erwan@gmail.com>
2024-02-16 09:54:26 +0100

    update image notfuound

--- REACT-UNIVERSOUND/src/pages/pageNotFound/pageNotFound.tsx
+++ REACT-UNIVERSOUND/src/pages/pageNotFound/pageNotFound.tsx
@@ -7,7 +7,7 @@ const PageNotFound: FunctionComponent = () => {
     return (
         <div className={styles.notfound}>
             <Link to="/songpage">-- Retourner à vos musiques --</Link>
-            <img src="../../images/notfound3.png" alt="" />
+            <img src="../cover/notfound3.png" alt="" />
             <h1>Oups!</h1>
             <h1>Ça sonne creux !</h1>
         </div>
