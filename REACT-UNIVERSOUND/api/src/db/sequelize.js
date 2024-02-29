d247e51ba349158f60a97f06b4d5bc943c3b7c52
ER1-53 <cremey.erwan@gmail.com>
2024-02-29 09:30:41 +0100

    add cors for production step in app.js error correction: -sequelize with DB add condition of verification data -audioplayer condition for duration of time for 0 & null added

--- REACT-UNIVERSOUND/api/src/db/sequelize.js
+++ REACT-UNIVERSOUND/api/src/db/sequelize.js
@@ -3,20 +3,29 @@ const SongModel = require('../models/song')
 const UserModel = require('../models/user')
 const songs = require('./mock-song')
 const bcrypt = require('bcrypt');
+const song = require('../models/song');
 
+let sequelize
 
-const sequelize = new Sequelize('universoundDB', 'universound', 'universoundHolberton', {
+if(process.env.NODE_ENV === 'production'){
+  sequelize = new Sequelize('universoundDB', 'universound', 'universoundHolberton', {
     host: 'localhost',
     dialect: 'mysql',
     logging: true,
-  }
-)
+  })
+} else {
+  sequelize = new Sequelize('universoundDB', 'universound', 'universoundHolberton', {
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false,
+  })
+}
 
 const Song = SongModel(sequelize, DataTypes)
 const User = UserModel(sequelize, DataTypes)
 
-const initDB = () => {
-  return sequelize.sync({force: true})
+/* const initDB = () => {
+  return sequelize.sync()
   .then(_ => {
     songs.map(song => {
     Song.create({
@@ -28,8 +37,29 @@ const initDB = () => {
         types: song.metadata.types,
         title: song.metadata.title,
         },
-    }).then(Bazock => console.log(Bazock.toJSON()))
-    })
+    }).then(song => console.log(song.toJSON()))
+    }) */
+
+const initDB = async () => {
+  const count = await Song.count();
+
+  if(count === 0) {
+    await sequelize.sync();
+
+    songs.map(song => {
+      Song.create({
+        audioSrc: song.audioSrc,
+        metadata: {
+          album: song.metadata.album,
+          artist: song.metadata.artist,
+          coverArtSrc: song.metadata.coverArtSrc,
+          types: song.metadata.types,
+          title: song.metadata.title,
+          },
+      })
+      .then(song => console.log(song.toJSON()))
+      .catch(error => console.error(error));
+
 
 
     bcrypt.hash('amazinger', 10)
@@ -45,9 +75,11 @@ const initDB = () => {
         .catch(error => console.error('Erreur lors de la création de l\'utilisateur :', error));
     })
     .catch(error => console.error('Erreur lors du hachage du mot de passe :', error));
-
+    })
+  } else {
     console.log('La base de données universoundDB est synchronisée.')
-  })
+    console.log(`Base de données déjà remplie.`)
+  }
 }
 
 module.exports = { initDB, Song, User }
