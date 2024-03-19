c29893b112bb45e96a0b2ae5c5f7c105da76ac02
ER1-53 <cremey.erwan@gmail.com>
2024-03-19 14:15:40 +0100

    update mock user for good id asign

--- REACT-UNIVERSOUND/api/src/db/mock-user.js
+++ REACT-UNIVERSOUND/api/src/db/mock-user.js
@@ -1,8 +1,8 @@
 
 const users = [
   {
-   id:1,
-   lastname: "admin",
+  id:1,
+  lastname: "admin",
   firstname: "admin",
   username: "admin",
   password: "admin",
@@ -11,24 +11,24 @@ const users = [
   resetPasswordExpires: null,
   },
   {
-    id:2,
-    lastname: "Gael",
-   firstname: "Deschamps",
-   username: "gael",
-   password: "phoenix",
-   mail: "stormphoenix@yopmail.com",
-   resetPasswordToken: null,
-   resetPasswordExpires: null,
-   },
-   {
-    id:3,
-    lastname: "Erwan",
-   firstname: "Cremey",
-   username: "erwan",
-   password: "erwan",
-   mail: "devmaster@yopmail.com",
-   resetPasswordToken: null,
-   resetPasswordExpires: null,
-   }
+  id:2,
+  lastname: "Gael",
+  firstname: "Deschamps",
+  username: "gael",
+  password: "phoenix",
+  mail: "stormphoenix@yopmail.com",
+  resetPasswordToken: null,
+  resetPasswordExpires: null,
+  },
+  {
+  id:3,
+  lastname: "Erwan",
+  firstname: "Cremey",
+  username: "erwan",
+  password: "erwan",
+  mail: "devmaster@yopmail.com",
+  resetPasswordToken: null,
+  resetPasswordExpires: null,
+  }
 ];
  module.exports = users;
